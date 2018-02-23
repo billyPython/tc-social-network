b0993c84a29aadb3631809c91508815a789f0760
danilo <danilo.vulicevic@yahoo.com>
2018-02-23 02:21:25 +0100

    Chane auth class in settings

--- settings/settings.py
+++ settings/settings.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
     ),
